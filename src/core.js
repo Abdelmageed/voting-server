fbb4a07db14d16fdfe07bace8dd8c2c1a91d1618
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-02-09 11:25:48 +0200

    Added getWinners() returns a list of the winning entries in a vote. next() returns a winner if it is the last entry.

--- src/core.js
+++ src/core.js
@@ -5,14 +5,31 @@ export const setEntries = (state, entries)=> {
 };
 
 export const next = (state)=> {
-  let entries = state.get('movies');
-  return state
-    .merge({
+  let entries = state.get('movies'); 
+  
+  entries = (state.has('vote') &&
+             state.get('vote').has('tally'))?
+      entries.concat(getWinners(state.get('vote'))) : entries;
+  
+  return (entries.count() > 1)? 
+    state.merge({
     movies: entries.skip(2),
     vote: Map({
       pair: entries.take(2)
     })
-  });
+    })
+    :
+    state.remove('vote')
+      .remove('movies')
+      .merge({
+    winner: entries.last()
+    });
+  
+};
+
+export const getWinners = (state)=> {
+  let tally = state.get('tally');
+  return tally.filter(entry => entry === tally.max()).keySeq();
 };
 
 export const vote = (state, entry)=> {
--- test/core.spec.js
+++ test/core.spec.js
@@ -1,4 +1,4 @@
-import {setEntries, next, vote} from '../src/core';
+import {setEntries, next, vote, getWinners} from '../src/core';
 import {expect} from 'chai';
 import {List, Map} from 'immutable';
 
@@ -40,7 +40,73 @@ describe('Application Logic', ()=> {
       
       expect(next(state)).to.equal(nextExpectedState);
     });
+    
+    it('should return the next state with the winning entry(ies) from the last vote placed at the end of entries and the loser is removed before preparing the next pair', ()=> {
+      let state = Map({
+        movies: List.of('Sunshine'),
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 5,
+            '28 Days Later': 4
+          })
+        })
+      }),
+          expectedNextState = Map({
+        movies: List(),
+        vote: Map({
+          pair: List.of('Sunshine', 'Trainspotting')
+        })
+      });
+      
+      expect(next(state)).to.equal(expectedNextState);
+    });
+    
+    it('should return the next state with the winner if only one entry is left in all entries', ()=> {
+      const state = Map({
+        movies: List(),
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 5,
+            '28 Days Later': 4
+          })
+        })
+      }),
+        expectedNextState = Map({
+        winner: 'Trainspotting'
+      });
+      
+      expect(next(state)).to.equal(expectedNextState);
+    })
   });
+  
+  describe('getWinners(vote)', ()=> {
+    
+    it('should return both entries on tie', ()=> {
+      const vote = Map({
+        tally: Map({
+          'Trainspotting': 4,
+          '28 Days Later': 4
+        })
+      }),
+            expectedWinners = List.of('Trainspotting', '28 Days Later');
+      
+      expect(getWinners(vote)).to.equal(expectedWinners);
+    })
+    
+    it('should return the winner', ()=> {
+      const vote = Map({
+        tally: Map({
+          'Trainspotting': 5,
+          '28 Days Later': 4
+        })
+      }),
+            expectedWinners = List.of('Trainspotting');
+      
+      expect(getWinners(vote)).to.equal(expectedWinners);
+    })
+  })
   
   describe('vote(state, entry)', ()=> {
     
